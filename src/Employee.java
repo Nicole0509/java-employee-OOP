f5f65d95658ae016dfbaf9684f7789dae9396554
Nicole0509 <niashavril0509@gmail.com>
2025-09-17 08:49:08 +0200

    Added a constructor to the Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,10 @@ public abstract class Employee {
 
     public String name;
 
+    public Employee(String name){
+        this.name=name;
+    }
+
     abstract public double calculateSalary();
 
     abstract public void displayInfo();
