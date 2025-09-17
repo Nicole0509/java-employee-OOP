af2990cc453d742ebd75df815e44119494a57e5f
Nicole0509 <niashavril0509@gmail.com>
2025-09-17 08:45:42 +0200

    Successfully implemented the properties of a manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,12 +1,21 @@
 public class Manager extends Employee{
 
+    double baseSalary;
+    double bonus;
+
+    public Manager(String name, double salary, double bonus){
+        super(name);
+        this.baseSalary=salary;
+        this.bonus=bonus;
+    }
+
     @Override
     public double calculateSalary() {
-        return 0;
+        return baseSalary + bonus;
     }
 
     @Override
     public void displayInfo() {
-        System.out.println("Manager");
+        System.out.println(name + " is a manager with a salary of " + baseSalary + " and bonus of " + bonus);
     }
 }
